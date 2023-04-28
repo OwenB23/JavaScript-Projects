8b6edf5311b0e54153e9cc708d074328b9c49916
OwenB23 <owenbond23@gmail.com>
2023-04-28 13:10:41 -0400

    Create main.js

---
+++ Project-2-functions/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function displayDate() {
+  document.getElementById("time").innerHTML = Date();
+}
+
+function myFunction() {
+  var sentence = "I am learning";
+  sentence += " alot from this book.";
+  document.getElementById("concatenate").innerHTML = sentence;
+}
