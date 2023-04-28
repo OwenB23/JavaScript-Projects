f899fa1187750683602831a130b489e872f160f1
OwenB23 <owenbond23@gmail.com>
2023-04-28 01:40:30 -0400

    First JS editing

---
+++ Project1_expressions_alert/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// This variable is defined as "sent1" and is concatenated with variable"sent2"
+const sent1 = "This is the beginning of the string ";
+const sent2 = "and this is the end of the string";
+
+document.write(sent1 + sent2);
+
+const alert = "Hello!";
+
+// Widnow.alert is an alert  that pops up when the user loads the page
+window.alert(alert);
+document.write(a);
+document.write(b);
+
+// This is an expression
+const exp = 2 + 2;
